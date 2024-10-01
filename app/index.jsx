35ad5e87cfeab18e205f31c5730345c84598aa43
Lucky02202 <laxmikant.patil02202@gmail.com>
2024-10-01 16:54:47 +0530

    Implemented  Inapp routing and designed Navigation bar for the project

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,23 @@
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
 
 const app = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">AORA</Text>
-      <StatusBar style="auto" />
-      <Link href={"/profile"} style={{ color: "blue", backgroundColor: "cyan" }}>Go to Profile</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center px-4">
+          <Image
+            className="w-[130px] h-[180px]"
+            resizeMode='contain'
+            source={images.logo}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
