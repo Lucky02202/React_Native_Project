05637084277abe26f7c1617882c30c150c2c5409
Lucky02202 <laxmikant.patil02202@gmail.com>
2024-10-01 17:14:58 +0530

    Created Starting screen and added some content in it.

--- app/index.jsx
+++ app/index.jsx
@@ -9,12 +9,31 @@ const app = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center px-4">
+        <View className="w-full justify-center items-center  px-4">
           <Image
-            className="w-[130px] h-[180px]"
-            resizeMode='contain'
             source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode='contain'
           />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{'\n'} Possibilities with
+              <Text className="text-secondary-200"> Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-7"
+              resizeMode='contain'
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where cretavity meets innovation: embark on a journey of limitless exploration with Aora
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
